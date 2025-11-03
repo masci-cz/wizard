53b24885eecba5aaa847d6654f9632d4d455349c
Daniel Masek <daniel@masci.cz>
2025-11-03 21:30:06 +0100

    Add dummy class

---
+++ src/main/java/cz/masci/wizard/step/Test.java
@@ -0,0 +1,11 @@
+package cz.masci.wizard.step;
+
+/**
+ * Dummy class to define the package.
+ */
+public class Test {
+
+    public static void main(String [] args) {
+        System.out.println("This is a dummy class to define the package.");
+    }
+}
